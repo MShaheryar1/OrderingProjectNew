847a88fcfa7939cdafb2189dab98e08084d74e72
Shez78 <shery_7@outlook.com>
2023-05-02 23:24:51 +0100

    Adding existing project to github

--- src/App.js
+++ src/App.js
@@ -4,23 +4,57 @@ import Menu2 from "./Components/Menu/Menu2";
 import CartProvider from "./Store/CartProvider";
 import Cart from "./Components/Cart/Cart";
 import React, { useState } from "react";
+import Login from "./Components/UserManagment/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./Components/UserManagment/Signup";
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   const showCartHandler = () => {
     setCartIsShown(true);
   };
+
   const hideCartHandler = () => {
     setCartIsShown(false);
   };
 
+  const authenticateHandler = () => {
+    setIsAuthenticated(true);
+  };
+
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />;
-      <main>
-        <Menu2 />
-      </main>
-    </CartProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/login"
+          element={<Login onAuthenticate={authenticateHandler} />}
+        />
+        <Route
+          path="/signup"
+          element={<Signup onAuthenticate={authenticateHandler} />}
+        />
+        {isAuthenticated ? (
+          <Route path="/" element={<Menu2 />} />
+        ) : (
+          <Route
+            path="/"
+            element={<Login onAuthenticate={authenticateHandler} />}
+          />
+        )}
+      </Routes>
+      <CartProvider>
+        {cartIsShown && <Cart onClose={hideCartHandler} />}
+        {isAuthenticated && <Header onShowCart={showCartHandler} />}
+        <main>
+          {isAuthenticated ? (
+            <Menu2 />
+          ) : (
+            <Login onAuthenticate={authenticateHandler} />
+          )}
+        </main>
+      </CartProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/UserManagment/Login.js
@@ -0,0 +1,94 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import validation from "./LoginValidation";
+import { useState } from "react";
+
+function Login({ onAuthenticate }) {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
+
+  const handleInput = (event) => {
+    setValues((prev) => ({ ...prev, [event.target.name]: event.target.value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors(validation(values));
+    if (errors.email === "" && errors.password === "") {
+      axios
+        .post("http://localhost:3000/login", values)
+        .then((res) => {
+          if (res.data === "Success") {
+            onAuthenticate();
+            navigate("/Home"); // navigate to Menu2 component
+          } else {
+            alert("No record existed");
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  };
+
+  const handlesignuphandler = () => {
+    navigate("/signup");
+  };
+
+  return (
+    <div
+      className="d-flex justify-content-center-align-items-center bg-priamry vh-100 paddingTop:50"
+      style={{ marginTop: "100px", marginLeft: "300px" }}
+    >
+      <div className="bg-white p-3 rounded w-25">
+        <form action="" onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Please enter your email"
+              name="email"
+              onChange={handleInput}
+              className="form-control rounded-0"
+            />
+            {errors.email && (
+              <span className="text-danger">{errors.email}</span>
+            )}
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Please enter your password"
+              name="password"
+              onChange={handleInput}
+              className="form-control rounded-0"
+            />
+            {errors.password && (
+              <span className="text-danger">{errors.password}</span>
+            )}
+          </div>
+          <button type="submit" className="btn btn-success w-100">
+            <strong>Login</strong>
+          </button>
+
+          <button
+            className="btn btn-default border w-100"
+            onClick={handlesignuphandler}
+          >
+            Create Account
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
